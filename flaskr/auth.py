aaf8aa9b5fb56f72e12c3af62953e59234728878
Leonardo Crespo <leonardo.crespo@nrg.com>
2021-11-26 17:39:34 -0600

    started separating out user and employee login and registration

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,7 +1,8 @@
 import functools
+from abc import ABC, abstractmethod
 
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app as app
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -9,9 +10,18 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
+class Auth(ABC):
+    @abstractmethod
+    def register(request):
+        pass
+
+    @abstractmethod
+    def login(request):
+        pass
+
+class UserAuth(Auth):
+    def register(self, request):
+        app.logger.info("Registering User")
         username = request.form['username']
         password = request.form['password']
         db = get_db()
@@ -32,15 +42,12 @@ def register():
             except db.IntegrityError:
                 error = f"User {username} is already registered."
             else:
-                return redirect(url_for("auth.login"))
+                return redirect(url_for("auth.user.login"))
 
         flash(error)
 
-    return render_template('auth/register.html')
-
-@bp.route('/login', methods=('GET', 'POST'))
-def login():
-    if request.method == 'POST':
+    def login(self, request):
+        app.logger.info("Login User")
         username = request.form['username']
         password = request.form['password']
         db = get_db()
@@ -61,7 +68,84 @@ def login():
 
         flash(error)
 
-    return render_template('auth/login.html')
+
+class EmployeeAuth(Auth):
+    def register(self, request):
+        app.logger.info("Registering Employee")
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.employee.login"))
+
+        flash(error)
+
+    def login(self, request):
+        app.logger.info("Login Employee")
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+@bp.route('/user/register', methods=('GET', 'POST'))
+def user_register():
+    if request.method == 'POST':
+        auth = UserAuth()
+        auth.register(request)
+    return render_template('auth/user/register.html')
+
+@bp.route('/user/login', methods=('GET', 'POST'))
+def user_login():
+    if request.method == 'POST':
+        auth = UserAuth()
+        auth.login(request)
+    return render_template('auth/user/login.html')
+
+
+@bp.route('/employee/register', methods=('GET', 'POST'))
+def user_register():
+    if request.method == 'POST':
+        auth = EmployeeAuth()
+        auth.register(request)
+    return render_template('auth/employee/register.html')
+
+@bp.route('/employee/login', methods=('GET', 'POST'))
+def user_login():
+    if request.method == 'POST':
+        auth = EmployeeAuth()
+        auth.login(request)
+    return render_template('auth/employee/login.html')
 
 
 @bp.before_app_request
